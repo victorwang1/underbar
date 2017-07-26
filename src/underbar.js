c52b322a9c339aec8595ff3d24021bb2a0095d54
Victor Wang <victor.wang@me.com>
2017-07-26 12:49:47 +1000

    implement function _.delay

--- src/underbar.js
+++ src/underbar.js
@@ -317,6 +317,7 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    setTimeout.apply(null, arguments);
   };
 
 
