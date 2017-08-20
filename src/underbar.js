7d52de1d450de013044b6830c00383703b8f38a2
Victor Wang <victor.wang@me.com>
2017-08-20 12:23:53 -0700

    refactor functions _.extend and _.defaults

--- src/underbar.js
+++ src/underbar.js
@@ -230,25 +230,23 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
-    return _.reduce(arguments, function(accumulator, object) {
-      _.each(object, function(value, key){
-        accumulator[key] = value;
-      });
-      return accumulator;
+    _.each(arguments, function(object) {
+      _.each(object, function(value, key) {
+        obj[key] = value;
+      })
     });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
-    return _.reduce(arguments, function(accumulator, object) {
+    _.each(arguments, function(object) {
       _.each(object, function(value, key) {
-        if (obj[key] === undefined)
-          accumulator[key] = value;
-        return accumulator;
-      });
-      return accumulator;
+        obj[key] === undefined && (obj[key] = value);
+      })
     });
+    return obj;
   };
 
 
